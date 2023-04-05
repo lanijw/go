0d2a0bb783e9b1ad632a230551a745fe249b75de
laniw <lani.julian.wagner+github@gmail.com>
2023-04-05 18:14:32 +0200

    Adds / to end of long url when redirecting.

--- newtab/newtab.js
+++ newtab/newtab.js
@@ -7,16 +7,20 @@ document.getElementById('shorthand-input').onkeydown = async e => {
     const {[storageKey]: mapping} = await chrome.storage.sync.get([storageKey]);
     const shorthand = e.target.value;
 
-    const matchingMapping = mapping[mapKey].filter(m => m[InputType.SHORT] !== '').find(m => shorthand.startsWith(m[InputType.SHORT]));
+    const matchingMapping = mapping[mapKey].filter(m => m[InputType.SHORT] !==
+        '').find(m => shorthand.startsWith(m[InputType.SHORT]));
     if (matchingMapping) {
-      const redirectUrl = matchingMapping[InputType.LONG] + shorthand.slice(matchingMapping[InputType.SHORT].length);
-      chrome.tabs.update({url: redirectUrl});
+      const redirectUrl = matchingMapping[InputType.LONG] +
+          shorthand.slice(matchingMapping[InputType.SHORT].length);
+      chrome.tabs.update({
+        url: redirectUrl.endsWith('/') ? redirectUrl : redirectUrl + '/'
+      });
     } else {
-      setErrorText(`The shorthand "${shorthand}" doesn't match a value or the matching value is an empty string.`)
+      setErrorText(`The shorthand "${shorthand}" doesn't match a value or the matching value is an empty string.`);
     }
   }
 };
 
 const setErrorText = (text) => {
   errorMsgContainer.innerText = text;
-}
+};
