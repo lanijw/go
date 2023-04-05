f1f069751b941b6433f191e2f632dba378603daa
laniw <lani.julian.wagner+github@gmail.com>
2023-04-05 18:09:24 +0200

    Styles newtab.

--- newtab/newtab.js
+++ newtab/newtab.js
@@ -1,17 +1,22 @@
 import {InputType, mapKey, storageKey} from '../util/util.js';
 
+const errorMsgContainer = document.getElementById('error-msg-container');
+
 document.getElementById('shorthand-input').onkeydown = async e => {
   if (e.key === 'Enter') {
     const {[storageKey]: mapping} = await chrome.storage.sync.get([storageKey]);
     const shorthand = e.target.value;
 
-    const matchingMapping = mapping[mapKey].find(m => shorthand.startsWith(m[InputType.SHORT]));
-    let redirectUrl;
-    if (!matchingMapping) {
-      redirectUrl = shorthand;
+    const matchingMapping = mapping[mapKey].filter(m => m[InputType.SHORT] !== '').find(m => shorthand.startsWith(m[InputType.SHORT]));
+    if (matchingMapping) {
+      const redirectUrl = matchingMapping[InputType.LONG] + shorthand.slice(matchingMapping[InputType.SHORT].length);
+      chrome.tabs.update({url: redirectUrl});
     } else {
-    redirectUrl = matchingMapping[InputType.LONG] + shorthand.slice(matchingMapping[InputType.SHORT].length);
+      setErrorText(`The shorthand "${shorthand}" doesn't match a value or the matching value is an empty string.`)
     }
-    chrome.tabs.update({url: redirectUrl});
   }
 };
+
+const setErrorText = (text) => {
+  errorMsgContainer.innerText = text;
+}
